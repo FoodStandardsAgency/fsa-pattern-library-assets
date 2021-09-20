2f95b079d7378354d46a4c65898a3881dffdc291
Peter Berryman <peterb@deeson.co.uk>
2021-09-20 12:22:48 +0000

    Update from https://github.com/FoodStandardsAgency/fsa-pattern-library-source/commit/7646e85e7d450c69ccc75758ba52cc2d5c79e7b3

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,7 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/******/ (() => { // webpackBootstrap
+/******/ (function() { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
@@ -14,9 +14,9 @@
 /*!****************************************************************!*\
   !*** ./src/pattern-library/components/Button/button.html.twig ***!
   \****************************************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"Button/button.html.twig\");\n\n//# sourceURL=webpack://fds/./src/pattern-library/components/Button/button.html.twig?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"Button/button.html.twig\");\n\n//# sourceURL=webpack://fds/./src/pattern-library/components/Button/button.html.twig?");
 
 /***/ }),
 
@@ -24,9 +24,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /*!**************************************************************************!*\
   !*** ./src/pattern-library/components/ListingItem/listingItem.html.twig ***!
   \**************************************************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"ListingItem/listingItem.html.twig\");\n\n//# sourceURL=webpack://fds/./src/pattern-library/components/ListingItem/listingItem.html.twig?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"ListingItem/listingItem.html.twig\");\n\n//# sourceURL=webpack://fds/./src/pattern-library/components/ListingItem/listingItem.html.twig?");
 
 /***/ }),
 
@@ -34,7 +34,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /*!***********************!*\
   !*** ./src/base.scss ***!
   \***********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://fds/./src/base.scss?");
 
@@ -44,7 +44,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /*!***********************************************************!*\
   !*** ./src/pattern-library/components/Button/button.scss ***!
   \***********************************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://fds/./src/pattern-library/components/Button/button.scss?");
 
@@ -54,7 +54,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /*!*********************************************************************!*\
   !*** ./src/pattern-library/components/ListingItem/listingItem.scss ***!
   \*********************************************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://fds/./src/pattern-library/components/ListingItem/listingItem.scss?");
 
@@ -64,7 +64,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _base_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base.scss */ \"./src/base.scss\");\n/* harmony import */ var _pattern_library_components_Button_Button__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./pattern-library/components/Button/Button */ \"./src/pattern-library/components/Button/Button.js\");\n/* harmony import */ var _pattern_library_components_ListingItem_ListingItem__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./pattern-library/components/ListingItem/ListingItem */ \"./src/pattern-library/components/ListingItem/ListingItem.js\");\n\n\n\n\n(0,_pattern_library_components_Button_Button__WEBPACK_IMPORTED_MODULE_1__.default)();\n(0,_pattern_library_components_ListingItem_ListingItem__WEBPACK_IMPORTED_MODULE_2__.default)();\n\n\n//# sourceURL=webpack://fds/./src/index.js?");
 
@@ -74,9 +74,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _bas
 /*!*********************************************************!*\
   !*** ./src/pattern-library/components/Button/Button.js ***!
   \*********************************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* export default binding */ __WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _button_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./button.scss */ \"./src/pattern-library/components/Button/button.scss\");\n/* harmony import */ var _button_html_twig__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./button.html.twig */ \"./src/pattern-library/components/Button/button.html.twig\");\n\n\n/* harmony default export */ function __WEBPACK_DEFAULT_EXPORT__() {\n  window.addEventListener('DOMContentLoaded', function () {\n    const buttons = document.querySelectorAll('.button--event');\n    if (!buttons.length) return;\n    buttons.forEach((button) => {\n      button.addEventListener('click', (e) => {\n        e.stopPropagation();\n        button.textContent = 'I was clicked';\n      });\n    });\n  });\n}\n\n\n//# sourceURL=webpack://fds/./src/pattern-library/components/Button/Button.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": function() { return /* export default binding */ __WEBPACK_DEFAULT_EXPORT__; }\n/* harmony export */ });\n/* harmony import */ var _button_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./button.scss */ \"./src/pattern-library/components/Button/button.scss\");\n/* harmony import */ var _button_html_twig__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./button.html.twig */ \"./src/pattern-library/components/Button/button.html.twig\");\n\n\n/* harmony default export */ function __WEBPACK_DEFAULT_EXPORT__() {\n  window.addEventListener('DOMContentLoaded', function () {\n    const buttons = document.querySelectorAll('.button--event');\n    if (!buttons.length) return;\n    buttons.forEach((button) => {\n      button.addEventListener('click', (e) => {\n        e.stopPropagation();\n        button.textContent = 'I was clicked';\n      });\n    });\n  });\n}\n\n\n//# sourceURL=webpack://fds/./src/pattern-library/components/Button/Button.js?");
 
 /***/ }),
 
@@ -84,9 +84,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /*!*******************************************************************!*\
   !*** ./src/pattern-library/components/ListingItem/ListingItem.js ***!
   \*******************************************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* export default binding */ __WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _listingItem_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./listingItem.scss */ \"./src/pattern-library/components/ListingItem/listingItem.scss\");\n/* harmony import */ var _listingItem_html_twig__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./listingItem.html.twig */ \"./src/pattern-library/components/ListingItem/listingItem.html.twig\");\n\n\n\n/* harmony default export */ function __WEBPACK_DEFAULT_EXPORT__() {\n\n  window.addEventListener('DOMContentLoaded', function () {\n    const cards = document.querySelectorAll('.listing-item__content');\n    if (!cards) return;\n\n    cards.forEach(card => {\n      card.addEventListener('click', (event) => {\n        event.stopPropagation();\n        const link = card.querySelector('.listing-item__title a');\n        link.click();\n      })\n    })\n  });\n}\n\n//# sourceURL=webpack://fds/./src/pattern-library/components/ListingItem/ListingItem.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": function() { return /* export default binding */ __WEBPACK_DEFAULT_EXPORT__; }\n/* harmony export */ });\n/* harmony import */ var _listingItem_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./listingItem.scss */ \"./src/pattern-library/components/ListingItem/listingItem.scss\");\n/* harmony import */ var _listingItem_html_twig__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./listingItem.html.twig */ \"./src/pattern-library/components/ListingItem/listingItem.html.twig\");\n\n\n\n/* harmony default export */ function __WEBPACK_DEFAULT_EXPORT__() {\n\n  window.addEventListener('DOMContentLoaded', function () {\n    const cards = document.querySelectorAll('.listing-item__content');\n    if (!cards) return;\n\n    cards.forEach(card => {\n      card.addEventListener('click', (event) => {\n        event.stopPropagation();\n        const link = card.querySelector('.listing-item__title a');\n        link.click();\n      })\n    })\n  });\n}\n\n//# sourceURL=webpack://fds/./src/pattern-library/components/ListingItem/ListingItem.js?");
 
 /***/ })
 
@@ -118,19 +118,19 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
+/******/ 	!function() {
 /******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 		__webpack_require__.d = function(exports, definition) {
 /******/ 			for(var key in definition) {
 /******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
 /******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
 /******/ 				}
 /******/ 			}
 /******/ 		};
-/******/ 	})();
+/******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
-/******/ 	(() => {
+/******/ 	!function() {
 /******/ 		__webpack_require__.g = (function() {
 /******/ 			if (typeof globalThis === 'object') return globalThis;
 /******/ 			try {
@@ -139,26 +139,26 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 				if (typeof window === 'object') return window;
 /******/ 			}
 /******/ 		})();
-/******/ 	})();
+/******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
+/******/ 	!function() {
 /******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
+/******/ 		__webpack_require__.r = function(exports) {
 /******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
 /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
-/******/ 	})();
+/******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
-/******/ 	(() => {
+/******/ 	!function() {
 /******/ 		var scriptUrl;
 /******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
 /******/ 		var document = __webpack_require__.g.document;
@@ -175,7 +175,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
 /******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
 /******/ 		__webpack_require__.p = scriptUrl;
-/******/ 	})();
+/******/ 	}();
 /******/ 	
 /************************************************************************/
 /******/ 	
