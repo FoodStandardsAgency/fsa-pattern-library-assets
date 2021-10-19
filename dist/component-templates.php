df87e04a3908d1189f640945997cd0061676c2c8
Peter Berryman <peterb@deeson.co.uk>
2021-10-19 13:37:09 +0000

    Update from https://github.com/FoodStandardsAgency/fsa-pattern-library-source/commit/8f3b2024863fba73741ed2db5e301d3a0ae38ff6

--- dist/component-templates.php
+++ dist/component-templates.php
@@ -11,14 +11,14 @@ function deeson_twig_component_templates($assetPath = 'auto') {
           "path" => $assetPath . "/layout/twoColumn",
           "variables" => ["content" => []]
         ], 
-        "formLayout" => [
-          "template" => "formLayout",
-          "path" => $assetPath . "/layout/formLayout",
-          "variables" => ["content" => []]
-        ], 
         "fullWidth" => [
           "template" => "fullWidth",
           "path" => $assetPath . "/layout/fullWidth",
+          "variables" => ["content" => []]
+        ], 
+        "formLayout" => [
+          "template" => "formLayout",
+          "path" => $assetPath . "/layout/formLayout",
           "variables" => ["content" => []]
         ], 
         "breadcrumb" => [
@@ -29,6 +29,11 @@ function deeson_twig_component_templates($assetPath = 'auto') {
         "button" => [
           "template" => "button",
           "path" => $assetPath . "/components/general/Button",
+          "variables" => ["content" => []]
+        ], 
+        "tabs" => [
+          "template" => "tabs",
+          "path" => $assetPath . "/components/general/Tabs",
           "variables" => ["content" => []]
         ], 
         "filtersSelected" => [
