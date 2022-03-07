a34d05040052b9a336439ee7a2e28b70089760d7
Peter Berryman <peterb@deeson.co.uk>
2022-03-07 17:13:03 +0000

    Update from https://github.com/FoodStandardsAgency/fsa-pattern-library-source/commit/b6d470d4107e217038876b3ca71526d5ae2f8890

--- dist/component-templates.php
+++ dist/component-templates.php
@@ -231,14 +231,14 @@ function deeson_twig_component_templates($assetPath = 'auto') {
           "path" => $assetPath . "/components/form/Textarea",
           "variables" => ["content" => []]
         ], 
-        "step" => [
-          "template" => "step",
-          "path" => $assetPath . "/components/form/Step",
-          "variables" => ["content" => []]
-        ], 
         "mapField" => [
           "template" => "mapField",
           "path" => $assetPath . "/components/form/MapField",
+          "variables" => ["content" => []]
+        ], 
+        "step" => [
+          "template" => "step",
+          "path" => $assetPath . "/components/form/Step",
           "variables" => ["content" => []]
         ], 
         "sortBy" => [
